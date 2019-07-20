e5120b6a31a8154566479a21b9cd5cdce01b8bb2
Kyle Luke <th.oldschool@gmail.com>
2019-07-20 14:38:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,6 @@ def square_array(array)
   counter = 0
   
   while counter < array.length do
-    puts array[counter]
     valueSquared = array[counter] ** 2
     puts valueSquared
     #array[counter] = valueSquared
